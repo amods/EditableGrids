92a5cbe030b5eea7468a5f4948b5408180a42a78
Amod shankar <amods@outlook.com>
2015-06-13 19:21:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿var gridApp = angular.module("gridApp", []);
+gridApp.controller('appController', function ($scope) {
+    $scope.clicked = true;
+    $scope.descriptionClicked = false;
+    $scope.titleClicked = false;
+
+    $scope.times = [
+        { i: { d: "Add Description", t: "Add Title", clicked: false }, j: { d: "Add Description", t: "Add Title", clicked: false }, k: { d: "Add Description", t: "Add Title", clicked: false } },
+        { i: { d: "Add Description", t: "Add Title", clicked: false }, j: { d: "Add Description", t: "Add Title", clicked: false }, k: { d: "Add Description", t: "Add Title", clicked: false } },
+        { i: { d: "Add Description", t: "Add Title", clicked: false }, j: { d: "Add Description", t: "Add Title", clicked: false }, k: { d: "Add Description", t: "Add Title", clicked: false } }
+    ]
+    $scope.linkClicked = function (column, linkType) {
+        column.clicked = true;
+        $scope.descriptionClicked = linkType === 'd' ? true : false;
+        $scope.titleClicked = linkType === 't' ? true : false;
+    }
+});
